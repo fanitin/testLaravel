a59bbd9add1b0ef2893c233885db6ae794d140c5
fanitin <kowtunwitalij@gmail.com>
2024-04-29 21:03:09 +0200

    testuje czy sie stworzylo repozytorium

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('main_content')
+    <p class="text-white">The RTL feature is still experimental and will probably evolve according to user feedback.
+         Spotted something or have an improvement to suggest? Open an issue, we'd love to get your insights.</p>
+    
+@endsection
